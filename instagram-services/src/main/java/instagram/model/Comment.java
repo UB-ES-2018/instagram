690cadcf394b1bc116545c6083c540d3e4197b17
Òscar <osleboa@gmail.com>
2018-10-23 17:19:14 +0200

    Añadido el servicio Comments

---
+++ instagram-services/src/main/java/instagram/model/Comment.java
@@ -0,0 +1,77 @@
+package instagram.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USER")
+public class Comment implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "ID")
+	@GeneratedValue(strategy = javax.persistence.GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "USERNAME")
+	private String username;
+
+	@Column(name = "PASSWORD")
+	private String password;
+
+	@Column(name = "EMAIL")
+	private String email;
+
+	@Column(name = "NAME")
+	private String name;
+	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
