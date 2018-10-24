d49e3963d4fe38a9f3c27c3b025588a486847490
abeljdrs <abeljdrs@hotmail.com>
2018-10-24 09:55:25 +0200

    [tram-ui] se ha enlazado el servicio de login con la UI

--- Instagram-ui/src/app/login/login.module.ts
+++ Instagram-ui/src/app/login/login.module.ts
@@ -3,9 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-    imports: [CommonModule, LoginRoutingModule],
-    declarations: [LoginComponent]
+    imports: [
+        CommonModule,
+        FormsModule,
+        NgbModule.forRoot(),
+        LoginRoutingModule
+    ],
+    declarations: [
+        LoginComponent
+    ]
 })
 export class LoginModule {}
