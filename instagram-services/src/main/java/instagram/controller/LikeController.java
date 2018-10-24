c11beae20cbd80b1bc82f7e0a19471173209e4f6
abeljdrs <abeljdrs@hotmail.com>
2018-10-24 22:13:14 +0200

    [MERGED BRAN ISSUE-45]

---
+++ instagram-services/src/main/java/instagram/controller/LikeController.java
@@ -0,0 +1,74 @@
+package instagram.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import instagram.controller.dto.ResponseDto;
+import instagram.controller.dto.LikeDto;
+import instagram.exception.BusinessException;
+import instagram.model.Like;
+import instagram.service.LikeService;
+
+@RestController
+@RequestMapping("/likes")
+public class LikeController {
+
+	private Logger logger = LoggerFactory.getLogger(LikeController.class);
+
+	@Autowired
+	private LikeService likeService;
+	
+	@CrossOrigin(origins = "http://localhost:4200")
+	@RequestMapping(value = "add", method = RequestMethod.POST)
+	public ResponseEntity<LikeDto> addLike(@RequestBody LikeDto likeDto) throws BusinessException {
+		logger.info("LikeController -> addLike");
+		
+		Like like = likeService.addLike(likeDto.getIdPost(), likeDto.getIdUser());
+
+		LikeDto likedDto = new LikeDto();
+		likedDto.loadFromModel(like);
+
+		return new ResponseEntity<LikeDto>(likedDto, HttpStatus.CREATED);
+	}
+
+	@RequestMapping(value = "/all", method = RequestMethod.GET)
+	public ResponseEntity<List<LikeDto>> getAll(){
+		logger.info("Post_TagController -> getAll");
+
+		List<Like> likes = likeService.getAll();
+
+		List<LikeDto> result = new ArrayList<LikeDto>();
+		for (Like like : likes) {
+			LikeDto likedDto = new LikeDto();
+			likedDto.loadFromModel(like);
+			result.add(likedDto);
+		}
+
+		return new ResponseEntity<List<LikeDto>>(result, HttpStatus.OK);
+	}
+
+	@RequestMapping(value ="/delete/{id}", method = RequestMethod.GET)
+	public ResponseEntity<ResponseDto> deleteLikeByID(@PathVariable int id) throws BusinessException{
+		logger.info("LikeController -> deleteLikeByID");
+				
+		this.likeService.deleteLike(id);
+		
+		ResponseDto responseDto = new ResponseDto();
+		responseDto.setOk(true);
+		
+		return new ResponseEntity<ResponseDto>(responseDto, HttpStatus.OK);
+				
+	}
+}
---
+++ instagram-services/src/main/java/instagram/service/impl/LikeServiceImpl.java
@@ -0,0 +1,52 @@
+package instagram.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.common.collect.Lists;
+
+import instagram.exception.BusinessException;
+import instagram.model.Like;
+import instagram.repository.LikeRepository;
+import instagram.service.LikeService;
+
+@Service
+public class LikeServiceImpl implements LikeService {
+
+	@Autowired
+	private LikeRepository likeRepository;
+
+	@Override
+	public Like getValidLikeByIdPost(int idPost) {
+		return likeRepository.findOneByIdPostIfIsValid(idPost);
+	}
+
+	@Override
+	public List<Like> getAll() {
+		return Lists.newArrayList(likeRepository.findAll());
+	}
+
+	@Override
+	public Like addLike(int idPost, int idUser) throws BusinessException {
+		Like like = new Like();
+		
+		like.setIdPost(idPost);
+		like.setIdUser(idUser);
+
+		likeRepository.save(like);
+
+		return like;
+	}
+
+	@Override
+	public void deleteLike(int id) throws BusinessException{
+		Optional<Like> optionalLike = this.likeRepository.findById(id);
+		Like like = optionalLike.get();
+		// Delete user itself
+		this.likeRepository.delete(like);
+	}
+
+}
