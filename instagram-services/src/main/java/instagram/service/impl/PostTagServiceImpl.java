3d36917a3fcc266703c34659cfd10732c70972bc
abeljdrs <abeljdrs@hotmail.com>
2018-10-24 22:08:05 +0200

    [MERGE WITH BRANCH ISSUE-44]

--- instagram-services/src/main/java/instagram/service/impl/PostTagServiceImpl.java
+++ instagram-services/src/main/java/instagram/service/impl/PostTagServiceImpl.java
@@ -3,15 +3,12 @@ package instagram.service.impl;
 import java.util.List;
 import java.util.Optional;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.google.common.collect.Lists;
 
 import instagram.exception.BusinessException;
-import instagram.exception.ErrorCodes;
 import instagram.model.PostTag;
 import instagram.repository.PostTagRepository;
 import instagram.service.PostTagService;
