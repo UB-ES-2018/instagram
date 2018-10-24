31728b04843d6e283427835471dbfd8fd21e500e
nafk1450 <albert_morales_94@hotmail.com>
2018-10-24 15:23:53 +0200

    Update UserController.java

--- instagram-services/src/main/java/instagram/controller/UserController.java
+++ instagram-services/src/main/java/instagram/controller/UserController.java
@@ -18,8 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 import instagram.exception.BusinessException;
 import instagram.model.User;
 import instagram.service.UserService;
-import mian.javainstagram.controller.dto.ResponseDto;
-import mian.javainstagram.controller.dto.UserDto;
 
 @RestController
 @RequestMapping("/users")
