1cd8700bf964d87d7f8d05af4c6dddef1755e61d
abeljdrs <abeljdrs@hotmail.com>
2018-10-26 13:42:02 +0200

    [tram-service] arreglado errores de Commet (model,Repository)

--- instagram-services/src/main/java/instagram/repository/CommentRepository.java
+++ instagram-services/src/main/java/instagram/repository/CommentRepository.java
@@ -9,10 +9,10 @@ import instagram.model.Comment;
 
 public interface CommentRepository extends CrudRepository<Comment, Integer> {
 
-	@Query("SELECT c FROM Comment c WHERE c.id_user = ?1")
+	@Query("SELECT c FROM Comment c WHERE c.idUser = ?1")
 	List<Comment> findAllByIdUser(int idUser);
 
-	@Query("SELECT c FROM Comment c WHERE c.id_post = ?1")
+	@Query("SELECT c FROM Comment c WHERE c.idPost = ?1")
 	List<Comment> findAllByIdPost(int idPost);
 
 }
