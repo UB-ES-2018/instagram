02589cdffc09c8e6bac8c1166722d469c7de9871
abeljdrs <abeljdrs@hotmail.com>
2018-10-26 13:18:34 +0200

    [instagram-ui] se ha agregado un servicio encargado de mantener el loging del usuario

--- Instagram-ui/src/app/login/login.component.ts
+++ Instagram-ui/src/app/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { routerTransition } from '../router.animations';
-import { UserService } from '../service/user.service';
 import { User } from '../model/User';
+import { authService } from '../service/auth.service';
 
 @Component({
     selector: 'app-login',
@@ -16,7 +16,7 @@ export class LoginComponent implements OnInit {
     username : string;
     user : User;
     constructor(public router: Router,
-                private userService: UserService ) {}
+                private authService: authService) {}
 
     ngOnInit() {
     }
@@ -29,7 +29,7 @@ export class LoginComponent implements OnInit {
         localStorage.setItem('isLoggedin', 'true');
     }
     onClickRegisterButton(){
-        this.userService.setLogin(this.username,this.password).subscribe(
+        this.authService.setLogin(this.username,this.password).subscribe(
             user => {
                 this.user = user
                 this.router.navigateByUrl('/perfil'); 
