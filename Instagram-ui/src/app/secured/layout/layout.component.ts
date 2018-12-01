1a80abe7680c455de3ee76a8ddc4b75aaecc4ce6
Arnau Vancells <hzover30@gmail.com>
2018-12-01 21:29:36 +0100

    Afegits pop-overs per a les notificacions

--- Instagram-ui/src/app/secured/layout/layout.component.ts
+++ Instagram-ui/src/app/secured/layout/layout.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { authService } from '../../service/auth.service';
+import { NgbPopover, NgbPopoverConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
+  
 })
 export class LayoutComponent implements OnInit {
+
+  //@ViewChild('popTitle') popTitle: ElementRef;
+  //@ViewChild('popContent') popContent: ElementRef;
+
   profilename: string;
-  constructor(private router: Router, public authenticationService: authService) { }
+  constructor(private router: Router, public authenticationService: authService) { 
+  }
   ngOnInit() {
     // this.profilename = this.authenticationService.logUser.name;
     if (this.authenticationService.logUser) {
@@ -32,4 +39,9 @@ export class LayoutComponent implements OnInit {
     const abuscar = document.getElementById(idbuscador).nodeValue;
     this.router.navigate([abuscar]);
   }*/
+
+  test(){
+    //this.popService.open(this.popContent, { centered: true, size: 'sm', windowClass: 'modal-cs' });
+    //this.popService.open()
+  }
 }
