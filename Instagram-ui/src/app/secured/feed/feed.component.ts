abcd69a9349caed8298f4250c62901f8c68217df
Joan Seguí Nieto <joanseguinieto@MacBook-Pro-de-Joan.local>
2018-12-02 17:34:58 +0100

    feed image

--- Instagram-ui/src/app/secured/feed/feed.component.ts
+++ Instagram-ui/src/app/secured/feed/feed.component.ts
@@ -1,5 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { ProfileService } from '../shared/profile.service';
+import { Profile } from '../shared/models/profile.model';
+import { UserService } from '../../service/user.service';
+import { User } from '../../model/User';
+import { Follow } from '../../model/Follow';
+import { authService } from '../../service/auth.service';
+import { FollowService } from '../../service/follow.service';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { loadQueryList } from '@angular/core/src/render3/instructions';
+import { PostService } from '../../service/post.service';
+import { PostLoad } from '../../model/PostLoad';
+import { ImageModalComponent } from '../image-modal/image-modal.component';
+import { PostPerfil } from '../../model/PostPerfil';
 
 @Component({
   selector: 'app-feed',
@@ -8,10 +21,28 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class FeedComponent implements OnInit {
 
-  constructor(private router: ActivatedRoute) { }
+  userID: number;
+  user: User = User.createDummy();
+  perfilPhotos: PostPerfil[];
+
+  constructor(private router: ActivatedRoute, private userService: UserService,
+    private ruta: Router, private authenticationService: authService,
+    private followService: FollowService, private modalService: NgbModal,
+    private postService: PostService) { }
 
   ngOnInit() {
-    
+    this.userID = this.authenticationService.logUser.id;
+    this.loadPhotosForPerfil(this.userID);
+  }
+
+  private loadPhotosForPerfil(idUser: number) {
+    this.postService.requestPhotosForPerfil(idUser).subscribe(
+      postPerfil => {
+        this.perfilPhotos = postPerfil;
+        this.perfilPhotos = this.perfilPhotos.slice().reverse()
+        console.log(this.perfilPhotos);
+      }
+    );
   }
 
 }
